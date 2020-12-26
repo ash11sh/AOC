034913fff807a4471f676c1bcce59cc364ad67da
ash11sh <ashish369@live.com>
2020-12-26 20:51:18 +0530

    code

--- 2p.py
+++ 2p.py
@@ -10,17 +10,11 @@ except:
     for i in lis:
         a,b,c = i
         l,h = a.split('-')
-
         if len(c)>=int(l) and len(c)>=int(h):
-
         	if c[int(l)-1]==b[0] and c[int(h)-1]!=b[0]:
-
         		count+=1
-
         	if c[int(h)-1]==b[0] and c[int(l)-1]!=b[0]:
-
         		count+=1
-
 
     print (count)
 
